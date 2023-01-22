c7911b7f14a82e0bf7040e6e0518597a3ab674d0
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2023-01-22 20:43:59 +0100

    Add files via upload

---
+++ Priprema_ispit_niz1/Priprema_ispit_niz1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema_ispit_niz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[10];
+            int[] arr2 = new int[10];
+            int x, n1 = 0, n2 = 0;
+            do
+            {
+                Console.WriteLine("Upišite broj:");
+                x = Convert.ToInt32(Console.ReadLine());
+                if (x % 2 != 0)
+                    arr1[n1++] = x;
+                else
+                    arr2[n2++] = x;
+            } while (x != 0 && n1 < 10 && n2 < 10);
+            Console.WriteLine();
+            for (int i = 0; i < n1; i++)
+                Console.WriteLine(arr1[i]);
+            Console.WriteLine();
+            for (int i = 0; i < n2; i++)
+                Console.WriteLine(arr2[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Priprema_ispit_niz2/Priprema_ispit_niz2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema_ispit_niz2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] arr2 = { 3, 2, 8, 5, 5, 7 };
+            int x, count = 0;
+            if (arr1.Length < arr2.Length)
+                x = arr1.Length;
+            else
+                x = arr2.Length;
+            for (int i = 0; i < x; i++)
+                if (arr1[i] == arr2[i])
+                    count++;
+            Console.WriteLine("Broj zajedničkih članova je {0}", count);
+
+            Console.ReadKey();
+        }
+    }
+}
